72d34f1dd798b853a488122b0577173d809957b1
刘锦帆 <aaron.me@foxmail.com>
2020-08-12 13:56:26 +0800

    add LiuViewController

---
+++ testxxx/LiuViewController.h
@@ -0,0 +1,17 @@
+//
+//  LiuViewController.h
+//  testxxx
+//
+//  Created by SeeWorld on 2020/8/12.
+//  Copyright © 2020 liujinfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LiuViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testxxx/LiuViewController.m
@@ -0,0 +1,32 @@
+//
+//  LiuViewController.m
+//  testxxx
+//
+//  Created by SeeWorld on 2020/8/12.
+//  Copyright © 2020 liujinfan. All rights reserved.
+//
+
+#import "LiuViewController.h"
+
+@interface LiuViewController ()
+
+@end
+
+@implementation LiuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
