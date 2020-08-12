c029233905879337740877b848c0931af4553289
刘锦帆 <aaron.me@foxmail.com>
2020-08-12 14:58:21 +0800

    1

---
+++ testxxx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testxxx
+//
+//  Created by SeeWorld on 2020/8/12.
+//  Copyright © 2020 liujinfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ testxxx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testxxx
+//
+//  Created by SeeWorld on 2020/8/12.
+//  Copyright © 2020 liujinfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
